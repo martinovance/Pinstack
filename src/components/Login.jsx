0355ecab423e130ca68639c047efb80d87f05590
unknown <martinsogunsina@gmail.com>
2023-01-24 13:11:15 +0600

    bug fixes

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { GoogleOAuthProvider, GoogleLogin } from '@react-oauth/google'
+import { GoogleLogin } from 'react-google-login'
 import { useNavigate } from 'react-router-dom'
 import { FcGoogle } from 'react-icons/fc';
 import shareVideo from '../assets/share.mp4';
@@ -11,24 +11,26 @@ const Login = () => {
 	const navigate = useNavigate();
 
 	const responseGoogle = (response) => {
-		localStorage.setItem('user', JSON.stringify(response.profileObj))
+		localStorage.setItem('user', JSON.stringify(response.profileObj));
 
-		const { name, googleId, imageUrl } = response.profileObj;
+		if (response.profileObj) {
+			const { name, googleId, imageUrl } = response.profileObj;
 
-		const doc = {
-			_id: googleId,
-			_type: 'user',
-			userName: name,
-			image: imageUrl,
+			const doc = {
+				_id: googleId,
+				_type: 'user',
+				userName: name,
+				image: imageUrl,
+			}
+
+			client.createIfNotExists(doc)
+				.then(() => {
+					navigate('/', { replace: true });
+				});
 		}
-
-		client.createIfNotExists(doc)
-		.then(() => {
-			navigate('/', { replace: true });
-		});
 	};
 
-	return(
+	return (
 		<div className="flex justify-start items-center flex-col h-screen">
 			<div className="relative w-full h-full">
 				<video
@@ -46,24 +48,22 @@ const Login = () => {
 					</div>
 
 					<div className="shadow-2xl">
-						<GoogleOAuthProvider>
-							<GoogleLogin
-								clientId={`${process.env.REACT_APP_GOOGLE_API_TOKEN}`}
-								render={(renderProps) => (
-									<button
-										type="button"
-										className="bg-mainColor flex justify-center items-center p-3 rounded-lg cursor-pointer outline-none"
-										onClick={renderProps.onClick}
-										disabled={renderProps.disabled}
-									>
-										<FcGoogle className="mr-4" /> Sign in with google
-									</button>
-								)}
-								onSuccess={responseGoogle}
-								onFailure={responseGoogle}
-								cookiePolicy="single_host_origin"
-							/>
-						</GoogleOAuthProvider>
+						<GoogleLogin
+							clientId={`${process.env.REACT_APP_GOOGLE_API_TOKEN}`}
+							render={(renderProps) => (
+								<button
+									type="button"
+									className="bg-mainColor flex justify-center items-center p-3 rounded-lg cursor-pointer outline-none"
+									onClick={renderProps.onClick}
+									disabled={renderProps.disabled}
+								>
+									<FcGoogle className="mr-4" /> Sign in with google
+								</button>
+							)}
+							onSuccess={responseGoogle}
+							onFailure={responseGoogle}
+							cookiePolicy="single_host_origin"
+						/>
 					</div>
 				</div>
 			</div>
--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { GoogleOAuthProvider, googleLogout } from '@react-oauth/google';
+import { GoogleLogout } from 'react-google-login';
 import { AiOutlineLogout } from 'react-icons/ai';
 import { useNavigate, useParams } from 'react-router-dom';
 
@@ -8,14 +8,19 @@ import { client } from '../client';
 import MasonryLayout from './MasonryLayout';
 import Spinner from './Spinner';
 
+const activeBtnStyles = "bg-red-500 text-white font-bold p-2 rounded-full w-20 outline-none";
+const notActiveBtnStyles = "bg-primary mr-4 text-black font-bold p-2 rounded-full w-20 outline-none";
+
 const UserProfile = () => {
-	const [user, setUser] = useState(null);
-	const [pins, setPins] = useState(null);
-	const [text, setText] = useState('');
-	const [activeBtn, setActiveBtn] = useState(false);
+	const [user, setUser] = useState();
+	const [pins, setPins] = useState();
+	const [text, setText] = useState('Created');
+	const [activeBtn, setActiveBtn] = useState('created');
 
 	const navigate = useNavigate();
 	const { userId } = useParams();
+
+	const User = localStorage.getItem('user') !== 'undefined' ? JSON.parse(localStorage.getItem('user')) : localStorage.clear();
 
 	useEffect(() => {
 		const query = userQuery(userId);
@@ -27,7 +32,7 @@ const UserProfile = () => {
 	}, [userId]);
 
 	useEffect(() => {
-		if(text === 'createed') {
+		if (text === 'created') {
 			const createdPinsQuery = userCreatedPinsQuery(userId);
 
 			client.fetch(createdPinsQuery)
@@ -44,18 +49,15 @@ const UserProfile = () => {
 		}
 	}, [text, userId])
 
-	const Logout = () => {
+	const logout = () => {
 		localStorage.clear();
 
 		navigate('/login');
-	}
+	};
 
-	if(!user) {
+	if (!user) {
 		return <Spinner message="Loading profile..." />
 	}
-
-	const activeBtnStyles = "bg-red-500 text-white font-bold p-2 rounded-full w-20 outline-none";
-	const notActiveBtnStyles = "bg-primary mr-4 text-black font-bold p-2 rounded-full w-20 outline-none";
 
 	const randomImage = 'https://source.unsplash.com/1600x900/?nature,photography,technology';
 
@@ -64,12 +66,12 @@ const UserProfile = () => {
 			<div className="flex flex-col pb-5">
 				<div className="relative flex flex-col mb-7">
 					<div className="flex flex-col justify-center items-center">
-						<img 
+						<img
 							src={randomImage}
 							className="w-full h-370 2xl:h-510 shadow-lg object-cover"
 							alt="banner-pic"
 						/>
-						<img 
+						<img
 							className="rounded-full w-20 h-20 mt-10 shadow-xl object-cover"
 							src={user?.image}
 							alt="user-pic"
@@ -79,24 +81,22 @@ const UserProfile = () => {
 						{user?.userName}
 					</h1>
 					<div className="absolute top-0 z-1 right-0 p-2">
-						{userId === user?._id && (
-							<GoogleOAuthProvider>
-								<googleLogout
-									clientId={`${process.env.REACT_APP_GOOGLE_API_TOKEN}`}
-									render={(renderProps) => (
-										<button
-											type="button"
-											className="bg-white p-2 rounded-full cursor-pointer outline-none shadow-md"
-											onClick={renderProps.onClick}
-											disabled={renderProps.disabled}
-										>
-											<AiOutlineLogout color="red" fontSize={21} /> Logout
-										</button>
-									)}
-									onLogoutSuccess={Logout}
-									cookiepolicy="single_host_origin"
-								/>
-							</GoogleOAuthProvider>
+						{userId === User?.googleId && (
+							<GoogleLogout
+								clientId={`${process.env.REACT_APP_GOOGLE_API_TOKEN}`}
+								render={(renderProps) => (
+									<button
+										type="button"
+										className="bg-white p-2 rounded-full cursor-pointer outline-none shadow-md"
+										onClick={renderProps.onClick}
+										disabled={renderProps.disabled}
+									>
+										<AiOutlineLogout color="red" fontSize={21} />
+									</button>
+								)}
+								onLogoutSuccess={logout}
+								cookiePolicy="single_host_origin"
+							/>
 						)}
 					</div>
 				</div>
@@ -122,14 +122,14 @@ const UserProfile = () => {
 						Saved
 					</button>
 				</div>
-				
-				{pins?.length ? (
-					<div className="px-2">
-						<MasonryLayout pins={pins} />
-					</div>
-				) : (
-					<div className="flex justify-center font-bold items-center w-full text-xl mt-2">
-						No pins found!
+
+				<div className="px-2">
+					<MasonryLayout pins={pins} />
+				</div>
+
+				{pins?.length === 0 && (
+					<div className="flex justify-center font-bold items-center w-full text-1xl mt-2">
+						No Pins Found!
 					</div>
 				)}
 
